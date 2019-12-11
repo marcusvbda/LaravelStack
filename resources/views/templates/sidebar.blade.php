4577b02b9b25b0df36675e9b02c6b22e31148c90
Vinicius Bassalobre <=>
2019-12-10 23:34:08 -0300

    merge

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -47,6 +47,7 @@ $('[data-toggle=collapse]').click(function(){
     return $(icon).addClass("el-icon-arrow-down")
 })
 $(".nav-item.active").parent().parent().toggleClass("show")
-$(".nav-item.active").parent().parent().parent().find(".nav-link").find(".menu-title").find(".menu-arrow").find(".icon").toggleClass("el-icon-arrow-down")
+$(".nav-item.active").parent().parent().parent().children(".nav-link").children(".menu-title").children(".menu-arrow").children(".icon").toggleClass("el-icon-arrow-down")
+
 </script>
 @endsection
