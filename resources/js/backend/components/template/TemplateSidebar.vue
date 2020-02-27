39a95cdbbbfc08d53f6b288f914b556313510800
Vinicius Bassalobre <=>
2020-02-27 17:13:19 -0300

    atualização

--- resources/js/backend/components/template/TemplateSidebar.vue
+++ resources/js/backend/components/template/TemplateSidebar.vue
@@ -1,13 +1,16 @@
 <template>
     <transition name="fade">
-        <div v-bind:class="{'has-logo':logo || smalllogo}" class="sidebar" id="sidebar" v-if="showing" >
-            <logo :logo="logo" :smalllogo="smalllogo"   />
-            <el-menu :collapse="$root.sidebarCollapse" class="h-100" :default-active="activeMenu"
+        <div v-bind:class="{'has-logo':logo || smalllogo}" class="sidebar" id="sidebar">
+            <logo :logo="logo" :smalllogo="smalllogo" />
+            <el-menu
+                :collapse="$root.sidebarCollapse"
+                class="h-100"
+                :default-active="activeMenu"
                 :background-color="bgColor"
                 active-text-color="white!important"
                 text-color="#ffffff94"
-            >   
-                <template  v-for="(m,i) in menu">
+            >
+                <template v-for="(m,i) in menu">
                     <template v-if="m.items">
                         <el-submenu :index="String(i)">
                             <template slot="title">
@@ -16,8 +19,14 @@
                             </template>
                             <template v-for="(item,y) in m.items">
                                 <el-menu-item-group>
-                                    <a  @click="goTo(item.url)" style="text-decoration:unset!important;">
-                                        <el-menu-item :index="`${item.label}_${i}-${y}`" :active="setActive(`${item.label}_${i}-${y}`,item.active)">
+                                    <a
+                                        @click="goTo(item.url)"
+                                        style="text-decoration:unset!important;"
+                                    >
+                                        <el-menu-item
+                                            :index="`${item.label}_${i}-${y}`"
+                                            :active="setActive(`${item.label}_${i}-${y}`,item.active)"
+                                        >
                                             <i :class="`${item.icon}`"></i>
                                             <span slot="title">{{item.label}}</span>
                                         </el-menu-item>
@@ -26,8 +35,13 @@
                             </template>
                         </el-submenu>
                     </template>
-                    
-                    <el-menu-item v-else :index="`${m.label}_${String(i)}`" :active="setActive(`${m.label}_${String(i)}`,m.active)" @click="goTo(m.url)">
+
+                    <el-menu-item
+                        v-else
+                        :index="`${m.label}_${String(i)}`"
+                        :active="setActive(`${m.label}_${String(i)}`,m.active)"
+                        @click="goTo(m.url)"
+                    >
                         <a @click="goTo(m.url)" style="text-decoration:unset!important;">
                             <i :class="`${m.icon}`"></i>
                             <span>{{m.label}}</span>
@@ -41,55 +55,53 @@
 <script>
 import variables from '../../../../sass/backend/_variables.scss'
 export default {
-    props : ["menu","logo","smalllogo"],
+    props: ["menu", "logo", "smalllogo"],
     data() {
         return {
-            showing : true,
-            activeMenu : ""
+            activeMenu: ""
         }
     },
-    components : {
-        "logo" : require("./partials/-logo.vue").default
+    components: {
+        "logo": require("./partials/-logo.vue").default
     },
-    computed : {
+    computed: {
         bgColor() {
             return variables ? variables.secondary : null
         },
     },
-    methods : {
-        setActive(index,active) {
-            if(active) this.activeMenu = index
+    methods: {
+        setActive(index, active) {
+            if (active) this.activeMenu = index
         },
         goTo(url) {
-            this.showing = false
-            window.location.href= url
+            window.location.href = url
         }
     }
 
 }
 </script>
 <style lang="scss">
-@import '../../../../sass/backend/_variables.scss';
+@import "../../../../sass/backend/_variables.scss";
 .el-menu--vertical {
     .el-menu-item-group {
         .el-menu-item-group__title {
-            padding : 0!important;
+            padding: 0 !important;
         }
     }
 }
 .el-menu {
-    &.el-menu--popup{
+    &.el-menu--popup {
         &.el-menu--popup-right-start {
-            background-color : $quaternary!important;
+            background-color: $quaternary !important;
             .el-menu-item {
-               background-color : $quaternary!important; 
-               &:hover {
-                   i {
-                       transition :.5s;
-                       color : white!important;
-                   }
-                   color : white!important;
-               }
+                background-color: $quaternary !important;
+                &:hover {
+                    i {
+                        transition: 0.5s;
+                        color: white !important;
+                    }
+                    color: white !important;
+                }
             }
         }
     }
@@ -99,14 +111,14 @@ export default {
         .el-submenu__icon-arrow {
             display: none;
         }
-        border-right : unset;
+        border-right: unset;
         width: 201px;
         &.el-menu--collapse {
-            width : unset;
-            .el-submenu__title { 
+            width: unset;
+            .el-submenu__title {
                 &:hover {
                     .el-icon-menu {
-                        color : white!important;
+                        color: white !important;
                     }
                 }
             }
@@ -114,11 +126,12 @@ export default {
         .el-submenu {
             &.is-active {
                 .el-submenu__title {
-                    background-color : $primary!important;
-                    color : white!important;
-                    font-weight : bolder;
-                    a,i {
-                        color : white!important;
+                    background-color: $primary !important;
+                    color: white !important;
+                    font-weight: bolder;
+                    a,
+                    i {
+                        color: white !important;
                     }
                     &:after {
                         right: 0;
@@ -137,25 +150,26 @@ export default {
             .el-submenu__title {
                 &:hover {
                     &:not(.is-active) {
-                        transition :.5s;
-                        color:white!important;
+                        transition: 0.5s;
+                        color: white !important;
                     }
                 }
             }
             .el-menu--inline {
                 width: 200px;
                 .el-menu-item-group {
-                    background-color:$quaternary;
+                    background-color: $quaternary;
                     .el-menu-item-group__title {
-                        padding : unset;
+                        padding: unset;
                     }
                     .el-menu-item {
-                        color : #ffffff94!important;
-                        background-color:$quaternary!important;
+                        color: #ffffff94 !important;
+                        background-color: $quaternary !important;
                         &.is-active {
-                            a,i {
-                                color : white!important;
-                                font-weight : bolder;
+                            a,
+                            i {
+                                color: white !important;
+                                font-weight: bolder;
                             }
                             &:after {
                                 right: 0;
@@ -164,9 +178,9 @@ export default {
                         }
                         &:hover {
                             &:not(.is-active) {
-                                background-color:$quaternary!important;
-                                transition :.5s;
-                                color:white!important;
+                                background-color: $quaternary !important;
+                                transition: 0.5s;
+                                color: white !important;
                             }
                         }
                     }
@@ -176,24 +190,25 @@ export default {
 
         .el-menu-item {
             a {
-                color : #ffffff94!important;
+                color: #ffffff94 !important;
                 &:hover {
                     &:not(.is-active) {
-                        transition :.5s;
-                        color:white!important;
+                        transition: 0.5s;
+                        color: white !important;
                         .el-icon-setting {
-                            transition :.5s;
-                            color:white!important;
+                            transition: 0.5s;
+                            color: white !important;
                         }
                     }
                 }
             }
             &.is-active {
-                background-color : $primary!important;
-                color : white!important;
-                a,i {
-                    color : white!important;
-                    font-weight : bolder;
+                background-color: $primary !important;
+                color: white !important;
+                a,
+                i {
+                    color: white !important;
+                    font-weight: bolder;
                 }
                 &:after {
                     right: 0;
@@ -210,12 +225,12 @@ export default {
             }
             &:hover {
                 &:not(.is-active) {
-                    transition :.5s;
-                    background-color : $tertiary!important;
-                    color:white!important;
+                    transition: 0.5s;
+                    background-color: $tertiary !important;
+                    color: white !important;
                     .el-icon-setting {
-                        transition :.5s;
-                        color:white!important;
+                        transition: 0.5s;
+                        color: white !important;
                     }
                 }
             }
@@ -239,23 +254,23 @@ export default {
             position: fixed;
             // height: calc(100vh + 61px);
             &.no-overflow {
-                overflow:  !important;
+                overflow: !important;
             }
             .el-menu {
-                width : 100%;
+                width: 100%;
             }
             .el-menu {
                 .el-menu--inline {
-                    width : 100%;
+                    width: 100%;
                 }
             }
         }
         .sidebar-logo-container {
-            display : none;
+            display: none;
         }
     }
     .no-overflow {
-        overflow-y : hidden;
+        overflow-y: hidden;
     }
 }
 </style>
